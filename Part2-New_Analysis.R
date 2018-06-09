0f085b7a427d97d161a00378854044a29cb43807
Nikita Gambhir <nikitagambhir03@gmail.com>
2018-06-09 17:34:50 -0500

    Ready for review

--- Part2-New_Analysis.R
+++ Part2-New_Analysis.R
@@ -6,8 +6,8 @@
 # 
 #  1. Working directories
 #  2. Reading in data
-#  3. Advanced data manipulation (filter, select, group_by, mutate)
-#  4. Finding the right tool for the job
+#  3. Advanced data manipulation (group_by, filter, mutate, select)
+#  4. Statistical Analysis
 #  
 #  
 # Part 1: Working Directories
@@ -40,26 +40,39 @@ list.files("data")
 # ------------------------
 # 
 # 
-# We can use the `read.table()` function to read these data in to R. It's
-# important to remember that while in R, these data are simply a copy kept *in
-# memory*, not on the disk, so we don't have to worry too much about
+# We can use the `read.csv()` function to read (or import) these data into R. 
+# It's important to remember that while in R, these data are simply a copy kept 
+# *in memory*, not on the disk, so we don't have to worry too much about
 # accidentally deleting the data :).
 # 
-# So, how do we actually USE the `read.table()` function? 
+# So, how do we actually USE the `read.csv()` function? 
 
-?read.table
+?read.csv
 
-# In order to read our data into R, we will need to provide three things:
-# 
-#  1. The path to the data set                 : data/fungicide_dat.csv
-#  2. If the first row are column names        : yes
-#  3. The separator for each cell in the data  : comma
-# 
-# Now that we have these elements, we can read the data into a variable, which
-# we can call "fungicide". Once we do this, we can check the dimensions to make
-# sure that we have all of the data.
+# You will notice that the help page describes all the sibling functions, too:
+# `read.table()`, `read.csv2()`, `read.delim()`, `read.delim2()`. Let's read the 
+# Description and Usage first. All arguments except 'file' have a default value
+# (e.g. header = TRUE). You do not need to specify these arguments unless you want 
+# to change the default. Let's use `read.csv()` with the default values.
 
-fungicide <- read.csv("data/fungicide_dat.csv", header = TRUE, sep = ",")
+read.csv("data/fungicide_dat.csv")
+
+# Let's use `read.table()` with the default values.
+read.table("data/fungicide_dat.csv")
+
+# This has two issues:
+# 1. The column names are treated as the first row. This is because the default 
+#    for header is FALSE in `read.table()`. We should change to header = TRUE. 
+# 2. The data is presented as one column, 'V1'. This is because the separator for 
+#    each cell in the data is a "," and not ""(space). We should change to sep = ",". 
+
+read.table("data/fungicide_dat.csv", header = TRUE, sep = ",")
+
+# Now that we have these elements, we can read the data into an object, which we
+# can call "fungicide". Once we do this, we can check the dimensions to make sure
+# that we have all of the data.
+
+fungicide <- read.csv("data/fungicide_dat.csv")
 nrow(fungicide)
 ncol(fungicide)
 
@@ -85,12 +98,9 @@ View(fungicide)
 # With these data, we want to answer the following questions:
 # 1. What is the mean yield of each treatment group in kg/ha?
 # 2. What is the percent severity of Control and Fungicide A?
-# 3. Control" | Treatment == "Fungicide_A"
-# 4. Mean_yield = mean(Yield_kg_per_ha))
-# 5. severity is measured in percent
-# 6. Which fungicide shows better results? (ANOVA)
+# 3. Which fungicide shows better results? (ANOVA)
 
-# Part 3: Advanced data manipulation (filter, select, group_by, mutate)
+# Part 3: Advanced data manipulation (group_by, filter, mutate, select)
 # ----------------------------------------------------------------------
 # The package 'dplyr' provides functions for easy and advanced data manipulation.
 # If we want to use it, we can download the package to our computer with the 
@@ -103,10 +113,15 @@ install.packages("dplyr")
 
 library("dplyr")
 
-# First, let's convert yield data from bu/acre to kg/ha. To do this conversion
-# for corn, we need to multiply the yield in bu/acre with 62.77. How can we add
-# a column with yield data in kg/ha? We can do this similar to what we learnt in 
-# Part 1.
+# 1. What is the mean yield of each treatment group in kg/ha?
+# -----------------------------------------------------------
+  
+# Let's go step by step to answer this:
+#
+# a) Convert yield data from bu/acre to kg/ha
+# To do this conversion for corn, we need to multiply the yield in bu/acre with
+# 62.77. So, how can we add a column with yield data in kg/ha? We can do this 
+# similar to what we learnt in Part 1.
 
 # fungicide$Yield_kg_per_ha <- fungicide$Yield_bu_per_acre*62.77
 
@@ -116,46 +131,61 @@ library("dplyr")
 # mutate(data, new_variable_name = calculation_based_upon_existing_variables)
 
 fungicide_1 <- mutate(fungicide, Yield_kg_per_ha = Yield_bu_per_acre*62.77)
-fungicide_1
 
 # > Note: We did not have to use fungicide$Yield_bu_per_acre. 
 
-# Now, we have two columns with yield and one with severity. We only want yield 
-# data and just in kg/ha. Let's select the yield column and create a new data  
-# frame. We can use the function `select()` that picks variables based on their 
-# names.
+# Let's print the data to see what we have
+fungicide_1
+
+# We have a new column `Yield_kg_per_ha`. We have two columns with yield and one 
+# with severity. We don't want severity data and want yield data only in kg/ha.
+
+# b) Create a new data frame with only `Treatment` and `Yield_kg_per_ha` columns
+# We can use the function `select()` that picks variables based on their names.
 
 fungicide_2 <- select(fungicide_1, Treatment, Yield_kg_per_ha)
 fungicide_2
 
-# Now, we can find the mean yield in kg/ha. If we want to summarise multiple values
-# to a single value, for example, mean, we can use the function `summarize()`.
+# c) Find the mean yield
+# If we want to summarise multiple values to a single value, for example, mean, 
+# we can use the function `summarize()`.
+
 summarize(fungicide_2, Mean_yield = mean(Yield_kg_per_ha))
 
 # Wait, we just got one mean even though we had three different groups (Control,
-# Fungicide_A, Fungicide_B). To perform any operation by group, we can use the
-# function `group_by()`.
+# Fungicide_A, Fungicide_B). We need to find the mean for every group. How can 
+# we tell R that it needs to group the data according to the treatment?
+
+# d) Group data according to treatment
+# We can use the function `group_by()` for this purpose. 
 
 fungicide_3 <- group_by(fungicide_2, Treatment)
-fungicide_3 # Yield column has underlined part because it is not printing the whole
-# answer (after decimals)
-# You will see that fungicide_2 and fungicide_3 don't have any difference because group_by 
-# does not do anything alone.
+fungicide_3 
 
-View(fungicide_3)
-
+# You will notice that `fungicide_2` and `fungicide_3` are a little different.
+# `fungicide_3` is also a "tibble", which is a form of data frame that gives 
+# more information about our data (e.g. what kind of data the columns are). You
+# will notice that Treatment is a factor, while Yield_kg_per_ha is a double 
+# (decimal). It also tells us that our data is grouped by Treatment (therefore, 
+# it has three groups). To add the grouping information to this data frame, 
+# 'dplyr' uses a sister package to convert it into a tibble. Also, note that 
+# `Yield_kg_per_ha` is not printing the whole answer (after decimals) and 
+# has underlined first two digits. The tibble format is different when we print
+# it, but the same when we use `View()`. The digits are underlined so that it
+# is easier to read larger numbers. It's role is similar to a comma. So,
+# '10,000' will have '10' underlined and '100,000' will have '100' underlined.
+# 
 #
-#
+# e) Find the mean yield of every group
 fungicide_4 <- summarize(fungicide_3, Mean_yield = mean(Yield_kg_per_ha))
 fungicide_4
 
-# Instead of creating different objects everytime, we may perform multiple functions
-# at once to create one final object. We use the pipe operator, %>% , for this purpose.
-# The left hand side of %>% acts as the input on which one function is performed and
-# the right hand side of %>% is where we can write a subsequent function to be performed.
-# A shortcut to write %>% is `ctrl+shift+m`.
+# Instead of creating different objects everytime, we can perform multiple functions
+# at once and create one final object. We use the pipe operator, %>% , for this purpose.
+# The left hand side of %>% acts as the input on which an operation on the right side
+# is performed. A shortcut to write %>% is `ctrl+shift+m`.
 
-yield_summary <- fungicide %>% #why is it broken/ what are the + signs
+yield_summary <- fungicide %>% 
   mutate(Yield_kg_per_ha = Yield_bu_per_acre*62.77) %>%  
   select(Treatment, Yield_kg_per_ha) %>% 
   group_by(Treatment) %>% 
@@ -163,12 +193,41 @@ yield_summary <- fungicide %>% #why is it broken/ what are the + signs
 
 yield_summary
 
-# What is the percent severity of Control and Fungicide A? First, we need to create a
-# new data frame where we only have severity data for Control and Fungicide A.
-# If we want to filter a data frame based upon specific values of a variable,
-# we can use the function `filter()`.
-# Let's make a new data frame for severity where severity is measured in percent and
-# not on a scale of 1 to 10.
+View(yield_summary)
+
+# We answered our first question!
+
+# You will notice that on the console below, the code for creating `yield_summary` has 
+# `+` signs before every line. The plus signs just indicate that R is waiting for the 
+# code to be finished. This sign also appears when we forget to put a closing
+# paranthesis ')'. Try this:
+
+str(yield_summary
+
+)
+
+# Let's go to our second question.
+
+# 2. What is the percent severity of Control and Fungicide A? 
+# -----------------------------------------------------------
+
+# First, we need to create a new data frame where we only have severity data for Control
+# and Fungicide A. If we want to filter a data frame based upon specific values of a 
+# variable, we can use the function `filter()`.
+
+filter(fungicide, Treatment == "Control" | Treatment == "Fungicide_A")
+
+# The treatment column should either be equal to (`==`) to Control OR (`|`) Fungicide_A.
+# We can also write the same expression as:
+
+filter(fungicide, Treatment != "Fungicide_B")
+
+# The treatment column should not be equal to (`!=`) to Fungicide_B. So, it will have 
+# everything except Fungicide B ("Control" and "Fungicide_A").
+
+# After filtering, we need to add a column `Percent_Severity`, where severity is measured
+# in percent and not on a scale of 1 to 10. Any guess on which function should we use?
+# Let's perform all operations in one go.
 
 severity_dat <- fungicide %>% 
   filter(Treatment == "Control" | Treatment == "Fungicide_A") %>% 
@@ -177,58 +236,54 @@ severity_dat <- fungicide %>%
 
 severity_dat
 
-# It can also be written as
-filter(fungicide, Treatment != "Fungicide_B") %>% 
-  mutate(Percent_Severity = Severity/10*100) %>% 
-  select(Treatment, Percent_Severity)
-
 # If we wanted to use these data as a table in a paper, we should export it to a
-# csv file. We do this using the function `write.table()`
+# csv file. We can do this using the function `write.table()`. Before that, let's
+# create a new directory (or folder) named results. We can do this using the
+# function `dir.create()`.
 
 dir.create("results")
 write.table(severity_dat, file = "results/severity_dat.csv", sep = ",", 
-            col.names = NA,
-            row.names = TRUE)
+            col.names = TRUE, 
+            row.names = FALSE)
 
-# Part 4: Finding the right tool for the job
-# -------------------------------------------
+# 3. Which fungicide shows better results? (ANOVA)
+#
+# Part 4: Statistical Analysis
+# ----------------------------
 # 
-# The dummy data presented here consists of yield (measured in bu/acre) and disease
-# severity (measured on a scale of 1 to 10) of a corn culitvar treated with two
-# fungicides. This trial was conducted to measure the efficacy of the two fungicides 
-# to manage disease. The experiment was laid out as a Completely Randomized Design.
-# With these data, we want to find which fungicide is effective? We can find this by
-# doing ANOVA for yield and severity data. The alpha level to be used is 0.05.
-# 
-# If you do an internet search for ANOVA in R, you will find that function `aov` is 
+# Let's do ANOVA for yield and severity data. We can use alpha level = 0.05. If we 
+# do an internet search for ANOVA in R, you will find that function `aov` is 
 # appropriate for this. How do we use `aov`?
 
 ?aov
 
-# This function takes in a formula to be tested. So if we want to find the response
-# of `Yield_bu_per_acre` (dependent variable) with respect to `Treatment` (independent 
-# variable), the formula should be written as 'Yield_bu_per_acre ~ Treatment'. The
-# function also takes in a data frame in which these variables are present. Other
-# arguments are optional.
+# This function takes in a formula to be tested. To find if the independent variable
+# (`Treatment`) had an effect on the dependent variable (`Yield_bu_per_acre`), the
+# formula should be written as 'dependent_variable ~ independent_variable'. If we 
+# used blocks in our experimental design, the formula can be modified to
+# 'dependent_variable ~ independent_variable + block'. The function also takes in a 
+# data frame in which these variables are present. 
 
-# Let's use this function
+# Let's use this function.
 aov(formula=Yield_bu_per_acre ~ Treatment, data=fungicide) 
 
 # As mentioned in the 'Description', `aov` fits an ANOVA model to an experimental design.
-# Let's check the 'Value' section of the help page so we know how to find the result.
+# Let's check the 'Value' section of the help page so we know what does it return. It
+# returns an object, which can further be explored with `print()` and `summary()`. Let's
+# assign it to an object so that we can use `print()` and `summary()` on it.
 
-# Create an object of class `aov`
 fit_yield <- aov(formula=Yield_bu_per_acre ~ Treatment, data=fungicide) 
 
-# Use `summary` method to find the 'Value'. 
-summary(fit_yield)
+print(fit_yield) 
 
-# 'Pr(>F)' is the associated p-value. As the p-value is less than 0.05, we can say that
-# the null hypothesis is rejected. So, at least one of the treatments is different.
+summary(fit_yield) # This is what we want!
 
-# Now that we know that differences exist between our treatments, how can we find which
-# treatments are different? We can do this by using Tukey's post-hoc test using the 
-# function `TukeyHSD`.
+# The output is similar to an ANOVA table. 'Pr(>F)' is the associated p-value. As the 
+# p-value is less than 0.05, the null hypothesis is rejected. So, at least one of the
+# treatments is different. Now that we know that differences exist between our 
+# treatments, how can we find which treatments are different? We can do this by using 
+# Tukey's post-hoc test. If you scroll down the help page of `aov` and go the section
+# 'See Also', you will see a function `TukeyHSD`. This is what we need.
 
 TukeyHSD(fit_yield) # There is no difference between 'Fungicide_A' and 'Control' as the
                     # p-value is 0.978 (>0.05)
@@ -236,5 +291,7 @@ TukeyHSD(fit_yield) # There is no difference between 'Fungicide_A' and 'Control'
 # Let's do similar analysis using the Severity data
 
 fit_severity <- aov(formula=Severity ~ Treatment, data=fungicide)
-summary(fit_severity)
-TukeyHSD(fit_severity) 
+summary(fit_severity) # p-value is less than 0.05, so let's do Tukey's post-hoc test
+
+TukeyHSD(fit_severity) # All the treatment pairs are significantly different from each
+# other as the p-value is < 0.05.
--- Part3-New_Visualization.R
+++ Part3-New_Visualization.R
@@ -63,10 +63,6 @@ stop("
 # colleagues (e.g. Alejandro Rojas:
 # https://github.com/alejorojas2/Rojas_Survey_Phytopath_2016).
 # 
-# It is important to note that ggplot2 uses "bare" column names, meaning that you 
-# do not need to put quotation marks when specifying a column.
-# 
-# 
 # 
 # 
 # Part 1: Creating our plot
@@ -81,7 +77,7 @@ stop("
 #  - `ggplot()` initializes a ggplot object from a data set. The data set needs
 #     to be a data frame.
 #  - `aes()` is a general way to specify what parts of the ggplot should be 
-#     mapped to variables in your data. What should be the x and y variables?
+#     mapped to variables in your data. e.g. What should be the x and y variables?
 # 
 # 
 # ### Creating the base of the ggplot
@@ -90,6 +86,8 @@ stop("
 # 
 #  1. The data set (fungicide)
 #  2. The mapping of the x and y coordinates (from the data set, using aes)
+
+# > Note, we can specify the column names without using quotation marks.
 
 yield.plot <- ggplot(data = fungicide, mapping = aes(x = Treatment, y = Yield_bu_per_acre))
 
@@ -104,9 +102,9 @@ yield.plot
 # 
 # To break down what the above function did, it first took in the data set 
 # `fungicide` and then mapped the x and y aesthetics to the Treatment and
-# yield columns. Effectively, this told ggplot how big our canvas needs to be
-# in order to display our data, but currently, it doesn't know HOW we want to
-# display our data; we need to give it a specific geometry.
+# Yield_bu_per_acre columns. Effectively, this told ggplot how big our canvas 
+# needs to be in order to display our data, but currently, it doesn't know 
+# HOW we want to display our data; we need to give it a specific geometry.
 # 
 # 
 # 
@@ -115,53 +113,55 @@ yield.plot
 # All functions that add geometries to data start with `geom_`, so if we wanted
 # the data to be displayed as a line showing the increase of yield over time,
 # we would use `geom_line()`. If we wanted to show the data displayed as points,
-# we can use `geom_point()`. We can also specify the color and shape of these
-# geometries using `aes()`.
+# we can use `geom_point()`. 
 # 
 # To add a geometry or anything to a ggplot object, we can just use the `+`
-# symbol. Here, we will add lines to the plot coloring them by Cultivar and
-# differing the line type by Experiment 
+# symbol. Here, we will add boxplots.
 # 
 # > Note: From here on out, I will be wrapping all commands with parentheses.
 # > This allows the result of the assignment to be displayed automatically.
 
 (yield.plot <- yield.plot + geom_boxplot())
 
-# If we want to color the plot accordng to Treatment, we can supply that 
-# information here. 
+# If we want to change the color of the boxplots from white (default) to orange,  
+# we can do this by adding `geom_boxplot(fill = "orange")`.
 
-(yield.plot <- yield.plot + geom_boxplot(aes(fill = Treatment)))
+(yield.plot <- yield.plot + geom_boxplot(fill = "orange"))
 
+# Instead of all the boxplots having the same color, it will be interesting if we 
+# could color them according to the Treatment.
 
-# Now you can see that we have a boxplot displaying the data, To give a title to
-# our plot, we can use `ggtitle()`. 
+(yield.plot <- yield.plot + geom_boxplot(fill = Treatment))
+
+# Oops! There was an error. It cannot recognize that we are talking about the 
+# Treatment column from our data set. This is because we have to use the function 
+# `aes()` whenever we are referring to our data set. 
+
+(yield.plot <- yield.plot + geom_boxplot(aes(fill = Treatment))) # This works!
+
+# To give a title to our plot, we can use `ggtitle()`. 
 
 (yield.plot <- yield.plot + ggtitle("Effect of Fungicides on Yield"))
 
-# We now have a fully functional and informative plot using only three lines of
+# We now have a fully functional and informative plot using only few lines of
 # code! Producing a visualization of your data can be an extremely useful tool
 # for analysis because it can allow you to see if there are any strange patterns
 # or spurious correlations in your variables. 
 # 
-# You can click on 'Zoom' to view a bigger version of this plot. You can also click
-# on 'Export' to save this plot as an image or pdf file on your computer.
+# We can click on 'Zoom' to view a bigger version of this plot. 
 #     
-# Of course, this plot is not quite publication ready. For one thing, the font is
-# too small and would cost a small fortune to include a color figure in a
-# journal. We need to add some customization.
-# 
+# Of course, this plot is not quite publication ready. We need to add some 
+# customization.
 # 
 # Preparing a publication-ready plot
 # ----------------------------------
 # 
-# In contrast to the above section, where layers are added to the plot, we are
-# now manipulating the aesthetics of the plot in how the data and labels are
+# Let's manipulate the aesthetics of the plot in how the data and labels are
 # displayed.
 # 
 # ### Axis labels 
 # 
-# We also need to update the axis labels. This is easily done with `xlab()` and
-# `ylab()`:
+# This is easily done with `xlab()` and `ylab()`:
 
 (yield.plot <- yield.plot + xlab("Treatment Applied"))
 (yield.plot <- yield.plot + ylab("Yield (bu/acre)"))
@@ -180,22 +180,20 @@ yield.plot
 
 # There are many different default themes available for ggplot2 objects that
 # change many aspects of the look and feel. The *ggthemes* contains many popular
-# themes such as fivethirtyeight and economist. Of course, as it is, the plot
-# is still not ready for publication. For one, the legend is taking up to much
-# horizontal realestate and the size of the plot is cutting off 
-# TwentyOneThirtySevenWheat.
+# themes such as fivethirtyeight and economist. Of course, we can make it
+# prettier before including it in our final manuscript.
 # 
 # To adjust granular aspects of the theme, we can use the `theme()` function, 
 # which contains a whopping 71 different options all related to the layout of
 # the non-data aspects of the plot.
 # 
 
+?theme
+
 stop("
      Look at ?theme and figure out one of the following:
      1. change the aspect ratio of the panels
      2. remove the background grid in the panels
-     3. change the placement of the legend
-     4. change the orientation of the legend
      ")
 
 # When we inspect the help page of the `theme()` function, we can find out how
@@ -204,23 +202,29 @@ stop("
 (yield.plot <- yield.plot + theme(aspect.ratio = 1.25)) 
 (yield.plot <- yield.plot + theme(panel.grid = element_blank()))
 
+# Since the information in the legend is repetetive, we can remove it. If you 
+# 'google' how to remove the legend in ggplot2, you will find that you can use
+# `guides(fill=FALSE)`.
+
+(yield.plot <- yield.plot + guides(fill=FALSE))
+
 # 
 # 
 # 
 # ### Putting it all together
 # 
 # Because we can add information to a plot with the `+` symbol, we can add all
-# of the elements in one go. Let's combine what we have above, but removing the
-# points and the color of the lines since these are redundant.
+# of the elements in one go. Let's combine what we have above.
 
 yield.plot <- ggplot(fungicide, aes(x = Treatment, y = Yield_bu_per_acre)) +
   geom_boxplot(aes(fill = Treatment)) +
   ggtitle("Effect of Fungicides on Yield") +
+  xlab("Treatment Applied") +
+  ylab("Yield (bu/acre)") +
   theme_bw(base_size = 14) +
   theme(aspect.ratio = 1.25) +
   theme(panel.grid = element_blank()) +
-  xlab("Treatment Applied") +
-  ylab("Yield (bu/acre)")
+  guides(fill=FALSE)
 yield.plot
 
 # We can now create a similar plot for Severity Data.
@@ -235,16 +239,20 @@ severity.plot <- ggplot(fungicide, aes(x = Treatment, y = Severity)) +
   ylab("Disease Severity")
 severity.plot
 
-# element_text() is an “element function” and it is used to format text elements.
-(severity.plot <- severity.plot + theme (plot.title = element_text(hjust = 0.5)))
+# The text of the title is not in the center. To format text elements of the
+# plot, we can use the function `element_text()` inside `theme()`. Since we
+# need to edit the text of the plot title, we need to specify 
+# `plot.title = element_text()`. Since we need to adjust the text horizontally 
+# in the center, we modify it to `theme(plot.title = element_text(hjust = 0.5))`.
 
-(severity.plot <- severity.plot + theme(axis.text.x = element_text(angle = 60, hjust = 1)))
+(severity.plot <- severity.plot + theme(plot.title = element_text(hjust = 0.5)))
 
 # 
-# Part 2: Saving your plot
+# Part 2: Saving our plot
 # 
 # Now that we have our plot finished, we can save it with the `ggsave()`
-# function, which allows us to save it as a pdf, png, svg, eps, etc. file. 
+# function, which allows us to save it as a pdf, png, svg, eps, etc. file.
+# Or, we can click on 'Export' and save it. 
 
 ggsave(filename = "results/figure1.pdf", width = 88, units = "mm")
 
